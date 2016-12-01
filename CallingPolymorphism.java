0c29eac676e226efecbd7868b4c73880542bc105
mahendra-16 <prajapatimahendra@ymail.com>
2016-12-01 21:49:44 +0530

    Update CallingPolymorphism.java

--- CallingPolymorphism.java
+++ CallingPolymorphism.java
@@ -5,11 +5,8 @@ public class CallingPolymorphism {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Area poly = new Area();
-		
-		System.out.println("Area of Square : " + poly.area(15));
-		
-		System.out.println("Area of Rectangle : " + poly.area(48, 64));
+		Area poly = new Area(45);
+		Area poly2 = new Area(45, 66);
 		
 	}
 
